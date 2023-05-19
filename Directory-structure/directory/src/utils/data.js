cb1bedf3baaef4e64de4588aace0eb6f1ad5490d
Joel Johny <76478362+Joel-Johny@users.noreply.github.com>
2023-05-19 21:14:52 +0530

    removing redundant data

--- Directory-structure/directory/src/utils/data.js
+++ Directory-structure/directory/src/utils/data.js
@@ -18,48 +18,7 @@ export const structure = [
             }
           ]
         },
-        {
-          id:3,
-          type:'folder',
-          name:'child with id 3',
-
-          nest:[
-            {
-              id:33,
-              type:'folder',
-              name:'child with id 33',
-              nest:[]
-            }
-          ]
-        },
-        {
-          id:4,
-          type:'folder',
-          name:'child with id 4',
-
-          nest:[
-            {
-              id:44,
-              type:'folder',
-              name:'child with id 44',
-              nest:[]
-            }
-          ]
-        },
-        {
-          id:5,
-          type:'folder',
-          name:'child with id 5',
-
-          nest:[
-            {
-              id:55,
-              type:'folder',
-              name:'child with id 55',
-              nest:[]
-            }
-          ]
-        },
+        
         {
           id:6,
           type:'file',
