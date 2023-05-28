3fe4fefed249c938a22bdaa7f5f7de46067c3ab3
Joel Johny <76478362+Joel-Johny@users.noreply.github.com>
2023-05-28 15:37:58 +0530

    fixed the bugs now the delete button will be shown if checkBox is checked

--- CheckBox/checkbox/src/App.jsx
+++ CheckBox/checkbox/src/App.jsx
@@ -5,21 +5,39 @@ import './App.css'
 function App() {
   const tasks1 =["Play Cricket","Play Videogame","Read Book"]
   const [tasks,setTasks]=useState(tasks1)
-
+  const [showDel,setShowDel]=useState([])
+  console.log(showDel)
+  function handleDelete(index){
+    console.log("deleting now",index)
+    const newTask=[...tasks]
+    newTask.splice(index,1)
+    setTasks(newTask)
+  }
   return (
     <>
       <ul className='taskBox'>
           {tasks.map((task,index)=>{
             return(
+              
               <li key={task} className='task'>
-                <input name="tasks" id={task} value={task} type="checkbox" />
+                <input name="tasks" id={task} value={task} type="checkbox" onChange={()=>setShowDel((oldState)=>{
+                  const newState=[...oldState]
+                  if(newState.includes(task))
+                  {
+                    console.log("Deleting index" ,index)
+                    const index1=newState.indexOf(task)
+                    newState.splice(index1,1)
+
+                  }
+                  else
+                    newState.push(task)
+                  return newState
+                })} />
+
                 <label htmlFor={task}>{task}</label>
-                <button onClick={()=>{
-                  const newTask=[...tasks]
-                  newTask.splice(index,1)
-                  setTasks(newTask)
-                }}>DELETE</button>
+                {(showDel.includes(task))&&<button onClick={()=>handleDelete(index)}>DELETE</button>}
               </li>
+ 
             )
           })}
       </ul>
